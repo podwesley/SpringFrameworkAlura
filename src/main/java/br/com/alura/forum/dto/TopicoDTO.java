82d55ad66ccf92288e2ce48ce20c9deec6dbbd7d
Wesley Soares <podwesley@gmail.com>
2019-06-19 01:50:35 -0300

    1º implementando o primeiro DTO.

---
+++ src/main/java/br/com/alura/forum/dto/TopicoDTO.java
@@ -0,0 +1,58 @@
+package br.com.alura.forum.dto;
+
+import br.com.alura.forum.domain.Topico;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopicoDTO {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataDeCriacao;
+
+    public TopicoDTO(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataDeCriacao = topico.getDataCriacao();
+    }
+
+    public static List<TopicoDTO> converterToTopicoDTO(List<Topico> topico) {
+        return topico.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+}
--- src/main/java/br/com/alura/forum/resources/TopicosResources.java
+++ src/main/java/br/com/alura/forum/resources/TopicosResources.java
@@ -2,10 +2,12 @@ package br.com.alura.forum.resources;
 
 import br.com.alura.forum.domain.Curso;
 import br.com.alura.forum.domain.Topico;
+import br.com.alura.forum.dto.TopicoDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,8 +16,8 @@ import java.util.List;
 public class TopicosResources {
 
     @GetMapping
-    public List<Topico> listarTodos(){
-        return Arrays.asList(new Topico("Duvida", "Duvida com spring", new Curso("Spring", "Programacao"))) ;
-    }
+    public List<TopicoDTO> listarTodos() {
 
+        return TopicoDTO.converterToTopicoDTO(Arrays.asList(new Topico("Dúvidas Spring Framework", "Como devo criar DTOS com o springframework?", new Curso("Spring", "Programação"))));
+    }
 }
